6f794a6cedb7615e232bde5973709a3d80720fa6
Gustavo Ciotto <gustavociotto@gmail.com>
2017-03-20 07:13:56 -0300

    script to set an appliance up

---
+++ docker-epics-appliance-mgmt/setup-appliance-specific.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -a
+set -e
+set -u
+
+. ${APPLIANCE_FOLDER}/build/scripts/env-vars.sh
+
+RAND_SRV_PORT=16000
+
+APPLIANCE_PORT=$(xmlstarlet sel -t -v "/appliances/appliance[identity='${ARCHAPPL_MYIDENTITY}']/${APPLIANCE_UNIT}_url" ${ARCHAPPL_APPLIANCES} | sed "s/.*://" | sed "s/\/.*//" ) 
+
+echo ${APPLIANCE_PORT}
+
+### Tomcat configuration
+# (i) Change tomcat's conf/ files
+xmlstarlet ed -L -u '/Server/@port' -v ${RAND_SRV_PORT} ${CATALINA_HOME}/conf/server.xml
+xmlstarlet ed -L -u "/Server/Service/Connector[@protocol='HTTP/1.1']/@port" -v ${APPLIANCE_PORT} ${CATALINA_HOME}/conf/server.xml
+xmlstarlet ed -L -d "/Server/Service/Connector[@protocol!='HTTP/1.1']" ${CATALINA_HOME}/conf/server.xml
+
+# (ii) Copy appliance into tomcat's webapps/
+mkdir ${CATALINA_HOME}/webapps/${APPLIANCE_UNIT}
+
+cp ${GITHUB_REPOSITORY_FOLDER}/${APPLIANCE_UNIT}.war ${CATALINA_HOME}/webapps/${APPLIANCE_UNIT}
+(cd ${CATALINA_HOME}/webapps/${APPLIANCE_UNIT}; jar xf ${APPLIANCE_UNIT}.war)
+
+rm ${CATALINA_HOME}/webapps/${APPLIANCE_UNIT}/${APPLIANCE_UNIT}.war
+
+ls ${CATALINA_HOME}/webapps/${APPLIANCE_UNIT}
+
+sed -i 's/username=.*$/username=\"'"${MYSQL_USER}"'\"/' ${CATALINA_HOME}/conf/context.xml
+sed -i 's/password=.*$/password=\"'"${MYSQL_PASSWORD}"'\"/' ${CATALINA_HOME}/conf/context.xml
+sed -i 's/url=.*$/url=\"jdbc:mysql:\/\/'"${HOST_ADDRESS}"':'"${MYSQL_PORT}"'\/'"${MYSQL_DATABASE}"'\"/' ${CATALINA_HOME}/conf/context.xml
