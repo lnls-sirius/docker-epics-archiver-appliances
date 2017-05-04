e8a3dc8c1eca19aa6562d0a403369311cf4cc40e
Gustavo Ciotto <gustavociotto@gmail.com>
2017-05-04 10:16:35 -0300

    github env vars moved to dockerfile

--- env-vars.sh
+++ env-vars.sh
@@ -4,10 +4,6 @@ DOCKER_MANTAINER_NAME=gciotto
 DOCKER_NAME=epics-archiver-generic
 DOCKER_RUN_NAME=epics-archiver-generic
 
-GITHUB_APPLIANCES_BRANCH=develop-php
-GITHUB_REPOSITORY_FOLDER=/opt/epicsarchiverap-ldap
-GITHUB_REPOSITORY_URL=https://github.com/lnls-sirius/epicsarchiverap-ldap.git
-
 MYSQL_USER=lnls_user
 MYSQL_PASSWORD=controle
 MYSQL_DATABASE=lnls_archiver_db
