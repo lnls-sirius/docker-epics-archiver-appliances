67a6917cdf50ed1e098787d3652dbd38a922c60e
Gustavo Ciotto <gustavociotto@gmail.com>
2017-05-04 10:16:09 -0300

    volumes creation moved to this script

--- build-docker-generic-appliance.sh
+++ build-docker-generic-appliance.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+#
+# A simple script to build a base image for the appliance containers.
+#
+# Gustavo Ciotto Pinton
+# Controls Group - Brazilian Synchrotron Light Source Laboratory - LNLS
+#
+
 . ./env-vars.sh
 
 docker build -t ${DOCKER_MANTAINER_NAME}/${DOCKER_NAME} .
