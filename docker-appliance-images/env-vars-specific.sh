4966924648e4847a0299b6e0fe9810ad2214d4d7
Gustavo Ciotto <gustavociotto@gmail.com>
2017-11-13 09:13:37 -0200

    update maintainer and added tag to image

--- docker-appliance-images/env-vars-specific.sh
+++ docker-appliance-images/env-vars-specific.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-DOCKER_MANTAINER_NAME=gciotto
+DOCKER_MANTAINER_NAME=lnlscon
 
 DOCKER_NAME=epics-archiver
 DOCKER_RUN_NAME=epics-archiver
+DOCKER_TAG=latest
 
 NETWORK_ID=epics-archiver-network
 
--- env-vars.sh
+++ env-vars.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-DOCKER_MANTAINER_NAME=gciotto
+DOCKER_MANTAINER_NAME=lnlscon
 DOCKER_NAME=epics-archiver-generic
 DOCKER_RUN_NAME=epics-archiver-generic
+DOCKER_TAG=latest
 
 MYSQL_USER=lnls_user
 MYSQL_PASSWORD=controle
 MYSQL_DATABASE=lnls_archiver_db
 MYSQL_PORT=3306
-
-HOST_ADDRESS=10.0.6.49
 
 NETWORK_ID=epics-archiver-network
